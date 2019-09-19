2725512ae1fdb412e40a676c9507d440352eea6f
Jonathan Dorety <kyullog@gmail.com>
2019-09-18 23:44:17 -0700

    Finish style and layout of Controls

--- src/components/Controls/Controls.jsx
+++ src/components/Controls/Controls.jsx
@@ -1,17 +1,19 @@
 import React from "react";
 import Button from "./Button/Button";
+import "nes.css/css/nes.min.css";
 import "./Controls.css";
 
 const Controls = () => {
-
   return (
-    <div className="button-wrapper">
-      <Button direction="&#8592" />
-      <div className="vertical-buttons">
-        <Button direction="&#8593" />
-        <Button direction="&#8595" />
+    <div className="controls-wrapper nes-container is-rounded">
+      <div className="button-wrapper">
+        <Button direction="&#8592" />
+        <div className="vertical-buttons">
+          <Button direction="&#8593" />
+          <Button direction="&#8595" />
+        </div>
+        <Button direction="&#8594" />
       </div>
-      <Button direction="&#8594" />
     </div>
   );
 };
