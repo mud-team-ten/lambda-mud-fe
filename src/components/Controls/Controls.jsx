9535afd66fb699262c35d94e064f1561ad86ea04
Jonathan Dorety <kyullog@gmail.com>
2019-09-19 14:55:02 -0700

    Fix arrows

--- src/components/Controls/Controls.jsx
+++ src/components/Controls/Controls.jsx
@@ -4,15 +4,21 @@ import "nes.css/css/nes.min.css";
 import "./Controls.css";
 
 function Controls() {
+  const dir = {
+    left: "&#11207",
+    right: "&#11208",
+    up: "&#11205",
+    down: "&#11206"
+  }
   return (
     <div className="controls-wrapper nes-container is-rounded">
       <div className="button-wrapper">
-        <Button direction="&#8592" />
+        <Button direction={dir.left} />
         <div className="vertical-buttons">
-          <Button direction="&#8593" />
-          <Button direction="&#8595" />
+          <Button direction={dir.up} />
+          <Button direction={dir.down} />
         </div>
-        <Button direction="&#8594" />
+        <Button direction={dir.right} />
       </div>
     </div>
   );
