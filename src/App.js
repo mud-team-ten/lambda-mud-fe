63120787de4723db20aa9262964314fea9493db4
Jonathan Dorety <kyullog@gmail.com>
2019-09-18 22:09:46 -0700

    Route auth pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route } from "react-router-dom";
+import Login from "./components/Authentication/Login/Login";
+import Register from "./components/Authentication/Register/Register";
+import logo from "./logo.svg";
+import "./App.css";
+import "nes.css/css/nes.min.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>Booo on You</h1>
+      <Route path="/login" component={Login} />
+      <Route path="/register" component={Register} />
     </div>
   );
 }
