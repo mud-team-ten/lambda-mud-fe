f83bd5b1632f2348c8a0451b0d0d115d7c0c9570
Jonathan Dorety <kyullog@gmail.com>
2019-09-21 11:02:30 -0700

    Add state and init and move

--- src/components/Game/index.js
+++ src/components/Game/index.js
@@ -1,11 +1,42 @@
 import React, { Component } from "react";
 import Controls from "./Controls/Controls";
+import config from "../../config";
 
 export default class Game extends Component {
+  state = {
+    uuid: "",
+    name: "",
+    title: "",
+    description: "",
+    players: []
+  };
+
+  movePlayer(direction) {
+    config
+      .axiosHeaders()
+      .post("/api/adv/move", { direction })
+      .then(res => console.log(res))
+      .catch(err => console.log(err));
+  }
+
+  componentDidMount() {
+    config
+      .axiosHeaders()
+      .get("/api/adv/init/")
+      .then(res => {
+        const { data } = res;
+        this.setState({
+          uuid: data.uuid,
+          name: data.name,
+          title: data.title,
+          description: data.description,
+          players: data.players
+        });
+      })
+      .catch(err => console.log(err));
+  }
 
   render() {
-      return (
-          <Controls />
-      )
+    return <Controls move={this.movePlayer} />;
   }
 }
